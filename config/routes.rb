4ca2ac5bf995a05ad7afaa3c958c972f8419914a
Sarah Kehm <sarah@k-e-h-m.de>
2020-02-27 12:16:35 +0800

    created CocktailsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+  root to: 'coktail#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
